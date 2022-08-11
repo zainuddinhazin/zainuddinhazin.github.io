ebd25ff02dd782a3c01e3cff72be301d76b7c10a
zai <98795942+zainuddinhazin@users.noreply.github.com>
2022-08-11 22:54:37 +0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -244,3 +244,29 @@ $(document).ready(function () {
     }
   );
 });
+
+//contact form
+$(".php-email-form").on("submit", function (e) {
+  //optional validation code here
+
+  e.preventDefault();
+
+  $.ajax({
+    url: "https://script.google.com/macros/s/AKfycbyBz_9oh4rj7AJfOglkyzevacfKhqDRFS3azRxKDSJH5AAV0XAPWfVfNaiN7ZhRH9m1/exec",
+    method: "POST",
+    dataType: "json",
+    data: $(".php-email-form").serialize(),
+    success: function (response) {
+      if (response.result == "success") {
+        $(".php-email-form")[0].reset();
+        alert("message has been sent successfully!");
+        return true;
+      } else {
+        alert("error error error! please contact me through email, thanks!");
+      }
+    },
+    error: function () {
+      alert("error error error! please contact me through email, thanks!");
+    },
+  });
+});
